9f00a2f35e8eb24873ae76b760e62dcdeeaf607b
Tom Christ <44477304+kiselblat@users.noreply.github.com>
2020-10-21 11:08:20 -0500

    Updates

---
+++ precache-manifest.23bf0476dc55f7e39059069ed54d430a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50683609032bd1ab177f697c4437a4b5",
+    "url": "/deerhead/index.html"
+  },
+  {
+    "revision": "81c235b0c90e6060c408",
+    "url": "/deerhead/static/css/main.da6663f7.chunk.css"
+  },
+  {
+    "revision": "e98878ce9599c9f4bbdf",
+    "url": "/deerhead/static/js/2.275c7dc9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/deerhead/static/js/2.275c7dc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81c235b0c90e6060c408",
+    "url": "/deerhead/static/js/main.ae1512b5.chunk.js"
+  },
+  {
+    "revision": "8ed93c7beeb19fefbdfd",
+    "url": "/deerhead/static/js/runtime-main.fc018162.js"
+  }
+]);
